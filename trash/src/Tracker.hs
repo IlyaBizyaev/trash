3ed406b0925abf6c469ffb9a268e9ef8f8c7a88e
Ilya Bizyaev <bizyaev@zoho.com>
2020-05-08 18:03:55 +0300

    Implement merge command

--- trash/src/Tracker.hs
+++ trash/src/Tracker.hs
@@ -24,7 +24,6 @@ import           PathUtils                      ( isChildOfPath
                                                 )
 import           FileSystem                     ( File(..)
                                                 , FileRevision(..)
-                                                , isDirTracked
                                                 , emptyTrackerData
                                                 , addRevisionsToTrackerData
                                                 , listFilesInDirEntry
@@ -35,6 +34,7 @@ import           FileSystem                     ( File(..)
 import           CommandHelpers                 ( makePathAbsolute
                                                 , getDirEntry
                                                 , forgetDirEntry
+                                                , replaceDirEntry
                                                 , modifyTrackerData
                                                 , getTrackerDirectory
                                                 , getTrackerData
@@ -42,6 +42,7 @@ import           CommandHelpers                 ( makePathAbsolute
                                                 )
 import           Data.List                      ( intercalate )
 import           Data.Either                    ( isRight )
+import qualified Data.ByteString               as B
 
 initCmd :: ExceptT CommandException (State ShellState) String
 initCmd = do
@@ -82,19 +83,21 @@ addCmd path summary = do
 logCmd :: FilePath -> ExceptT CommandException (State ShellState) String
 logCmd path = do
   fullPath    <- makePathAbsolute path
-  dirent <- getDirEntry path
+  dirent      <- getDirEntry path
   trackerPath <- getTrackerDirectory
   let filePaths = listFilesInDirEntry fullPath dirent
   trackerData <- getTrackerData
   let relativePaths = mapM (makeRelativeTo trackerPath) filePaths
   case relativePaths of
-    Nothing -> throwError UnknownException
+    Nothing       -> throwError UnknownException
     Just relPaths -> do
       let revisionLog = getLogFromTrackerData trackerData relPaths
       case revisionLog of
-        Nothing -> throwError UnknownException
+        Nothing     -> throwError UnknownException
         Just revLog -> do
-          let output = concatMap (\(ver, rs) -> ("# Revision " ++ show ver):(map show rs)) revLog
+          let output = concatMap
+                (\(ver, rs) -> ("# Revision " ++ show ver) : (map show rs))
+                revLog
           return $ intercalate "\n" output
 
 forgetCmd :: FilePath -> ExceptT CommandException (State ShellState) String
@@ -107,16 +110,17 @@ forgetRevCmd
 forgetRevCmd path rev = do
   fullPath    <- makePathAbsolute path
   trackerPath <- getTrackerDirectory
-  let relativePath  = makeRelativeTo trackerPath fullPath
+  let relativePath = makeRelativeTo trackerPath fullPath
   case relativePath of
-    Nothing -> throwError UnknownException
+    Nothing      -> throwError UnknownException
     Just relPath -> do
       modifyTrackerData (f relPath)
       return ""
-  where
-    f :: FilePath -> TrackerDataFunction
-    f _ Nothing = Left UnknownException
-    f p (Just trackerData) = case removeRevisionFromTrackerData trackerData p rev of
+ where
+  f :: FilePath -> TrackerDataFunction
+  f _ Nothing = Left UnknownException
+  f p (Just trackerData) =
+    case removeRevisionFromTrackerData trackerData p rev of
       Nothing -> Left UnknownException
       Just td -> return $ Just td
 
@@ -125,16 +129,16 @@ checkoutCmd
 checkoutCmd path version = do
   trackerData <- getTrackerData
   fullPath    <- makePathAbsolute path
-  dirent <- getDirEntry path
+  dirent      <- getDirEntry path
   when (isRight dirent) (throwError UnknownException)
   trackerPath <- getTrackerDirectory
-  let relativePath  = makeRelativeTo trackerPath fullPath
+  let relativePath = makeRelativeTo trackerPath fullPath
   case relativePath of
-    Nothing -> throwError UnknownException
+    Nothing      -> throwError UnknownException
     Just relPath -> do
       let mbRev = getRevisionFromTrackerData trackerData relPath version
       case mbRev of
-        Nothing -> throwError UnknownException
+        Nothing  -> throwError UnknownException
         Just rev -> return $ show rev
 
 mergeCmd
@@ -143,14 +147,38 @@ mergeCmd
   -> Integer
   -> String
   -> ExceptT CommandException (State ShellState) String
-mergeCmd path rev1 rev2 strategy = undefined
--- use: getRevisionFromTrackerData
--- ensure vcs dir is not Nothing
--- get full path of the target path
--- verify full path is a child/eq of tracker path
--- get that path relative to tracker path
--- get dirent for path (normalized relative? or works with absolute?)
--- get vcs dir
--- get tracker data
--- for file dirent: query specified revs, fail if one is not present, choose one based on strategy, write to file
--- for dir dirent: display error message
+mergeCmd path ver1 ver2 strategy = do
+  fullPath <- makePathAbsolute path
+  dirent   <- getDirEntry path
+  case dirent of
+    Right _    -> throwError UnknownException
+    Left  file -> do
+      trackerPath <- getTrackerDirectory
+      trackerData <- getTrackerData
+      let relativePath = makeRelativeTo trackerPath fullPath
+      case relativePath of
+        Nothing      -> throwError UnknownException
+        Just relPath -> do
+          let
+            revisions =
+              mapM (getRevisionFromTrackerData trackerData relPath) [ver1, ver2]
+          case revisions of
+            Just [firstRev, secondRev] -> do
+              newContent <- mergeWithStrategy firstRev secondRev strategy
+              let newDirent = Left $ file { fGetContent = newContent }
+              replaceDirEntry newDirent fullPath
+              return ""
+            _ -> throwError UnknownException
+ where
+  mergeWithStrategy
+    :: FileRevision
+    -> FileRevision
+    -> String
+    -> ExceptT CommandException (State ShellState) B.ByteString
+  mergeWithStrategy rev1 rev2 s = case s of
+    "left"  -> return $ frGetContent rev1
+    "right" -> return $ frGetContent rev2
+    "both"  -> return (B.append (frGetContent rev1) (frGetContent rev2))
+    _       -> throwError UnknownException
+
+
